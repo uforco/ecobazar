2b504e57da6a2cec927354c3b70b37703b4d3b5e
Sharif <srka780@gmail.com>
2024-11-13 17:59:09 +0600

    fix hover issue followus section

--- src/pages/home/followUs/FollowUs.tsx
+++ src/pages/home/followUs/FollowUs.tsx
@@ -1,15 +1,7 @@
 import React from 'react';
 import MaxWidthControls from '@/components/shared/MaxWidthControls';
 import Image from 'next/image';
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-  } from "@/components/ui/dialog"
-import { BrandInstagram } from '@mynaui/icons-react';
+import SingleContine from './SingleContine'
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
   
@@ -58,33 +50,3 @@ export default FollowUs;
 
 
 
-const SingleContine = ({data}: {data: { image: string, link: string }}) => {
-    return(
-       <div className=' cursor-pointer  flex justify-center ' >
-         <Dialog>
-            <DialogTrigger asChild >
-                <div className=' relative group overflow-hidden rounded-lg  w-[200px] h-[200px] ' >
-                    <div className=' absolute group-hover:top-0 transition-all rounded-lg duration-500 -top-[200px] left-0 w-full h-full bg-hard_primary/60 flex justify-center items-center text-white' >
-                    <BrandInstagram className=' w-12 h-12 ' ></BrandInstagram>
-                    </div>
-                    <Image src={data?.image} width={200} height={200} alt={''}></Image>
-                </div>
-            </DialogTrigger>
-            <DialogContent >
-                <DialogHeader>
-                    <DialogTitle>Instagram</DialogTitle>
-                </DialogHeader>
-                <div className=' flex justify-around gap-3 ' >
-                    <div >
-                        <Image className=' h-48 w-48 ' src={data?.image} alt={''} width={350} height={300} ></Image>
-                    </div>
-                    <div >
-                        <Image className=' h-48 w-48 ' src={data?.image} alt={''} width={350} height={300} ></Image>
-                    </div>
-                </div>
-            </DialogContent>
-        </Dialog>
-       </div>
-
-    )
-}
---
+++ src/pages/home/followUs/SingleContine.tsx
@@ -0,0 +1,46 @@
+'use client'
+import Image from 'next/image';
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+import { BrandInstagram } from '@mynaui/icons-react';
+
+const SingleContine = ({data}: {data: { image: string, link: string }}) => {
+    return(
+       <div className=' cursor-pointer  flex justify-center ' >
+         <div>
+            <Dialog>
+                <DialogTrigger >
+                    <div className=' relative group overflow-hidden rounded-lg  w-[200px] h-[200px] ' >
+                        <div className=' absolute group-hover:top-0 transition-all rounded-lg duration-500 -top-[200px] left-0 w-full h-full bg-hard_primary/60 flex justify-center items-center text-white' >
+                            <BrandInstagram className=' w-12 h-12 ' ></BrandInstagram>
+                        </div>
+                        <Image src={data?.image} width={200} height={200} alt={''}></Image>
+                    </div>
+                </DialogTrigger>
+                <DialogContent >
+                    <DialogHeader>
+                        <DialogTitle>Instagram</DialogTitle>
+                    </DialogHeader>
+                    <div className=' flex justify-around gap-3 ' >
+                        <div >
+                            <Image className=' h-48 w-48 ' src={data?.image} alt={''} width={350} height={300} ></Image>
+                        </div>
+                        <div >
+                            <Image className=' h-48 w-48 ' src={data?.image} alt={''} width={350} height={300} ></Image>
+                        </div>
+                    </div>
+                </DialogContent>
+            </Dialog>
+         </div>
+       </div>
+
+    )
+}
+
+export default SingleContine
