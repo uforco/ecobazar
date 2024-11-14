21e999e17e6632dc05b4e2e28e35ea0884f8286f
Sharif <srka780@gmail.com>
2024-11-14 19:19:11 +0600

    add user icon in manu ber

--- src/components/shared/header/HeaderMenusSection/HeaderMenuSection.tsx
+++ src/components/shared/header/HeaderMenusSection/HeaderMenuSection.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import MaxWidthControls from '@/components/shared/MaxWidthControls';
 import ProfileAccount from "./profile/ProfileAccount";
+import { HiOutlineShoppingBag } from "react-icons/hi2";
 
 const HeaderMenuSection = () => {
   return (
@@ -8,7 +9,10 @@ const HeaderMenuSection = () => {
     <MaxWidthControls>
       <div className=" flex justify-between items-center " >
           <div>fxtksedt</div>
-          <div>
+          <div className=" flex items-center gap-2 " >
+            <div className=" bg-black text-white text-2xl p-2 overflow-hidden relative rounded-full flex justify-center items-center " >
+              <HiOutlineShoppingBag></HiOutlineShoppingBag>
+            </div>
             <ProfileAccount></ProfileAccount>
           </div>
       </div>
--- src/components/shared/header/HeaderMenusSection/profile/ProfileAccount.tsx
+++ src/components/shared/header/HeaderMenusSection/profile/ProfileAccount.tsx
@@ -12,12 +12,15 @@ import {
   DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import Image from "next/image";
 
 const ProfileAccount = () => {
   return (
     <div>
       <DropdownMenu>
-        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuTrigger className=" border bg-black text-white rounded-full ">
+            <UserIcon></UserIcon>
+        </DropdownMenuTrigger>
         <DropdownMenuContent>
           <DropdownMenuLabel>My Account</DropdownMenuLabel>
           <DropdownMenuSeparator />
@@ -27,20 +30,19 @@ const ProfileAccount = () => {
           <DropdownMenuItem>Subscription</DropdownMenuItem>
 
           {/* <DropdownMenuGroup> */}
-            <DropdownMenuItem>Team</DropdownMenuItem>
-            <DropdownMenuSub>
-              <DropdownMenuSubTrigger>Invite users</DropdownMenuSubTrigger>
-              <DropdownMenuPortal>
-                <DropdownMenuSubContent>
-                  <DropdownMenuItem>Email</DropdownMenuItem>
-                  <DropdownMenuItem>Message</DropdownMenuItem>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem>More...</DropdownMenuItem>
-                </DropdownMenuSubContent>
-              </DropdownMenuPortal>
-            </DropdownMenuSub>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>Invite users</DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+                <DropdownMenuItem>Email</DropdownMenuItem>
+                <DropdownMenuItem>Message</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>More...</DropdownMenuItem>
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
           {/* </DropdownMenuGroup> */}
-          
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
@@ -48,3 +50,17 @@ const ProfileAccount = () => {
 };
 
 export default ProfileAccount;
+
+const UserIcon = () => {
+  return (
+    <div className=" p-2 size-[39px] flex justify-center items-center ">
+      <Image
+        width={62}
+        height={62}
+        src={"/images/navBer/userIcon.png"}
+        alt={""}
+      >
+      </Image>
+    </div>
+  );
+};
