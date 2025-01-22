b84701b43a5d2195004e74e46a8993aa7812e8db
MD Sharif <srka780@gmail.com>
2025-01-22 17:44:29 +0600

    chack error page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,14 @@ import dynamic from 'next/dynamic'
 // import HomePage from "@/pages/home/HomePage";
 import HomePage from "@/pages/homeTwo/HomePage";
 const PopupAds = dynamic(()=> import('@/components/shared/popupAds/PopupAds') )
-import Testimonial from './about/testimonial/Testimonial';
 
 
 export default function Home() {
   return (
     <div>
-      {/* <PopupAds></PopupAds> */}
+      <PopupAds></PopupAds>
       {/* <HomePage></HomePage>  */}
       <HomePage />
-
-      {/* <Testimonial></Testimonial> */}
-
-
     </div>
   );
 }
