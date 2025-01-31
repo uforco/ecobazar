f582afa15980b7f2cd36e6a6cf2a8931ab75f649
MD Sharif <srka780@gmail.com>
2025-01-31 18:24:18 +0600

    sometiong change quick view card

--- src/components/shared/quick_product_view/quick_view/MediaSection.tsx
+++ src/components/shared/quick_product_view/quick_view/MediaSection.tsx
@@ -6,19 +6,19 @@ interface Props {}
 function MediaSection(props: Props) {
     const {} = props
     return (
-        <div className=" flex justify-between items-center w-full " >
-          <div className=" flex flex-col gap-1 " >
-                <div className=" cursor-pointer border border-Primary transition-all rounded p-1 flex justify-center items-center " >
-                    <Image className=" w-20 h-20 " src={"/images/categores/productImage.png"} width={80} height={80} alt={""} ></Image>
+        <div className=" flex justify-between gap-2 items-center w-full " >
+          <div className=" flex flex-col gap-2 " >
+                <div className=" w-16 h-16 overflow-hidden p-2 cursor-pointer border border-Primary transition-all rounded flex justify-center items-center " >
+                    <Image  src={"/images/categores/productImage.png"} width={100} height={100} alt={""} ></Image>
                 </div>
-                <div className=" cursor-pointer border border-transparent hover:border-Primary transition-all rounded p-1 flex justify-center items-center " >
-                    <Image className=" w-20 h-20 "  src={"/images/categores/productImage1.png"} width={100} height={100} alt={""}></Image>
+                <div className=" w-16 h-16 overflow-hidden p-2 cursor-pointer border border-transparent hover:border-Primary transition-all rounded flex justify-center items-center " >
+                    <Image   src={"/images/categores/productImage1.png"} width={100} height={100} alt={""}></Image>
                 </div>
-                <div className=" cursor-pointer border border-transparent hover:border-Primary transition-all rounded p-1 flex justify-center items-center " >
-                    <Image className=" w-20 h-20 " src={"/images/categores/productImage2.png"} width={80} height={80} alt={""}></Image>
+                <div className=" w-16 h-16 overflow-hidden p-2 cursor-pointer border border-transparent hover:border-Primary transition-all rounded flex justify-center items-center " >
+                    <Image  src={"/images/categores/productImage2.png"} width={100} height={100} alt={""}></Image>
                 </div>
-                <div className=" cursor-pointer border border-transparent hover:border-Primary transition-all rounded p-1 flex justify-center items-center " >
-                    <Image className=" w-20 h-20 " src={"/images/categores/productImage3.png"} width={80} height={80} alt={""}></Image>
+                <div className=" w-16 h-16 overflow-hidden p-2 cursor-pointer border border-transparent hover:border-Primary transition-all rounded flex justify-center items-center " >
+                    <Image  src={"/images/categores/productImage3.png"} width={100} height={100} alt={""}></Image>
                 </div>       
           </div>
           <div>
