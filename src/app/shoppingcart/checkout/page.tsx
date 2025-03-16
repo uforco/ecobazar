bc896c358c591c125d9c2f532b51816be302ad35
MD Sharif <srka780@gmail.com>
2025-03-16 20:49:17 +0600

    resolve error fix

---
+++ src/app/shoppingcart/checkout/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            checkout
+        </div>
+    );
+};
+
+export default page;
--- src/app/shoppingcart/page.tsx
+++ src/app/shoppingcart/page.tsx
@@ -1,31 +1,37 @@
-import MaxWidthControls from '@/components/shared/MaxWidthControls';
-
-import React from 'react'
-import { GoHome } from 'react-icons/go';
-import { IoIosArrowForward } from 'react-icons/io';
-import ProductTable from './productTable/ProductTableMainComponent';
-
+import React from "react";
+import MaxWidthControls from "@/components/shared/MaxWidthControls";
+import { GoHome } from "react-icons/go";
+import { IoIosArrowForward } from "react-icons/io";
+import ProductTable from "./shoppingcartSystem/productTable/ProductTableMainComponent";
 
 interface Props {}
 
 function Page(props: Props) {
-    const {} = props
+  const {} = props;
 
-    return (
-        <div>
-            <div className=" w-full relative overflow-hidden bg-origin-content h-28 md:bg-cover bg-no-repeat bg-[url('/images/categores/Breadcrumbs.png')]  " >
-                <div className='  w-full h-full ' >
-                    <MaxWidthControls className='h-full'>
-                        <div className=" flex items-center h-full  gap-2 text-base font-poppins text-gray-400 " >
-                            <span className=' text-xl ' ><GoHome></GoHome></span> <IoIosArrowForward></IoIosArrowForward> <span className=' text-Primary ' >Shopping</span>
-                        </div>
-                    </MaxWidthControls>
-                </div>
+  return (
+    <div>
+      <div className=" w-full relative overflow-hidden bg-origin-content h-28 md:bg-cover bg-no-repeat bg-[url('/images/categores/Breadcrumbs.png')]  ">
+        <div className="  w-full h-full ">
+          <MaxWidthControls className="h-full">
+            <div className=" flex items-center h-full  gap-2 text-base font-poppins text-gray-400 ">
+              <span className=" text-xl ">
+                <GoHome></GoHome>
+              </span>
+              <IoIosArrowForward></IoIosArrowForward>
+              <span className=" text-Primary ">Shopping</span>
             </div>
-            <div className=' w-full ' > <h2 className=' w-full text-center text-2xl my-8' >My Shopping Cart</h2> </div>
-            <ProductTable></ProductTable>
+          </MaxWidthControls>
         </div>
-    )
+      </div>
+      <div className=" w-full ">
+        <h2 className=" w-full text-center text-2xl my-8">
+          My Shopping Cart
+        </h2>
+      </div>
+      <ProductTable></ProductTable>
+    </div>
+  );
 }
 
-export default Page
+export default Page;
